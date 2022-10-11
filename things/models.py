da6d04e9dafe0e6d0a8591957da90114ebdcf234
Ben Banks <benjamin.banks960@gmail.com>
2022-10-11 09:44:24 +0100

    Keyword arg fix

--- things/models.py
+++ things/models.py
@@ -4,7 +4,7 @@ from django.core.validators import MinValueValidator, MaxValueValidator
 # Create your models here.
 class Thing(models.Model):
     # By default blank=False
-    things = models.CharField(max_length=30, unique=True)
+    name = models.CharField(max_length=30, unique=True)
     # By default unique=False
-    name = models.CharField(blank=True, max_length=120,)
+    description = models.CharField(blank=True, max_length=120)
     quantity = models.IntegerField(validators=[MinValueValidator(0), MaxValueValidator(100)])
