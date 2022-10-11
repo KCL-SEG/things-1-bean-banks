99a0f7e34369981852abbbb0a805c1497e93d301
Ben Banks <benjamin.banks960@gmail.com>
2022-10-11 09:50:30 +0100

    Kwargs fix2

--- things/models.py
+++ things/models.py
@@ -4,7 +4,7 @@ from django.core.validators import MinValueValidator, MaxValueValidator
 # Create your models here.
 class Thing(models.Model):
     # By default blank=False
-    name = models.CharField(max_length=30, unique=True)
+    name = models.CharField(max_length=30, unique=True, blank=False)
     # By default unique=False
-    description = models.CharField(blank=True, max_length=120)
-    quantity = models.IntegerField(validators=[MinValueValidator(0), MaxValueValidator(100)])
+    description = models.CharField(max_length=120, blank=True, unique=False)
+    quantity = models.IntegerField(unique=False, validators=[MinValueValidator(0), MaxValueValidator(100)])
